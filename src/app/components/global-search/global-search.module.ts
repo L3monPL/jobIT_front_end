9f92dba925a5ff2e70fae37654569c1f17d70086
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2023-01-20 15:44:45 +0100

    change globalSearch

--- src/app/components/global-search/global-search.module.ts
+++ src/app/components/global-search/global-search.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalSearchComponent } from './global-search.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -9,7 +10,8 @@ import { GlobalSearchComponent } from './global-search.component';
     GlobalSearchComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [
     GlobalSearchComponent
