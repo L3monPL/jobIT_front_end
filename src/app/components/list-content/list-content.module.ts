f98244ab68a44db775984a7264fe6434cc372b97
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-03-02 10:42:12 +0100

    change

--- src/app/components/list-content/list-content.module.ts
+++ src/app/components/list-content/list-content.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListContentComponent } from './list-content.component';
-
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { ListContentComponent } from './list-content.component';
     ListContentComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [
     ListContentComponent
