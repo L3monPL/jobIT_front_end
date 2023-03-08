d11e1e075eab7b5ed2682cbe0101e6919dbe93d1
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-03-08 09:29:06 +0100

    init routing / login

--- src/app/components/main-content/main-content.module.ts
+++ src/app/components/main-content/main-content.module.ts
@@ -5,6 +5,31 @@ import { GlobalSearchModule } from '../global-search/global-search.module';
 import { ListContentModule } from '../list-content/list-content.module';
 import {MatIconModule} from '@angular/material/icon';
 import { LoginModule } from '../login/login.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    // children: [
+    //   {
+    //     path: 'login',
+    //     component: LoginComponent,
+    //   },
+    //   {
+    //     path: 'remind-password',
+    //     component: RemindPasswordComponent,
+    //   },
+    //   {
+    //     path: 'register',
+    //     component: RegisterComponent,
+    //   },
+    // ]},
+  },
+  
+  { path: '**',   redirectTo: '', pathMatch: 'full' },
+];
 
 
 @NgModule({
@@ -16,7 +41,8 @@ import { LoginModule } from '../login/login.module';
     GlobalSearchModule,
     ListContentModule,
     MatIconModule,
-    LoginModule
+    LoginModule,
+    RouterModule.forRoot(routes),
   ],
   exports: [
     MainContentComponent
