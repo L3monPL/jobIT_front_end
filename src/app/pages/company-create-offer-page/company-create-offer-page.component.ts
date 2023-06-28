e1e9845a0ec7c95a62d9215c8936b8f98c65b4f8
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-06-28 14:28:27 +0200

    change /company-offer-create style of show information

--- src/app/pages/company-create-offer-page/company-create-offer-page.component.ts
+++ src/app/pages/company-create-offer-page/company-create-offer-page.component.ts
@@ -18,4 +18,48 @@ export class CompanyCreateOfferPageComponent {
     this.router.navigateByUrl('./company')
   }
 
+  stackForm(stackForm: any): Array<any>{
+    let array = []
+    for (let index = 0; index < stackForm.length; index++) {
+      console.log(stackForm[index].name)
+      if (stackForm[index].name != '') {
+        array.push(stackForm[index])
+      }
+    }
+    return array
+  }
+
+  additionalStackForm(additionalStackForm: any): Array<any>{
+    let array = []
+    for (let index = 0; index < additionalStackForm.length; index++) {
+      console.log(additionalStackForm[index].name)
+      if (additionalStackForm[index].name != '') {
+        array.push(additionalStackForm[index])
+      }
+    }
+    return array
+  }
+
+  responsibilitiesForm(responsibilitiesForm: any): Array<any>{
+    let array = []
+    for (let index = 0; index < responsibilitiesForm.length; index++) {
+      console.log(responsibilitiesForm[index].name)
+      if (responsibilitiesForm[index].name != '') {
+        array.push(responsibilitiesForm[index])
+      }
+    }
+    return array
+  }
+
+  expectationsForm(expectationsForm: any): Array<any>{
+    let array = []
+    for (let index = 0; index < expectationsForm.length; index++) {
+      console.log(expectationsForm[index].name)
+      if (expectationsForm[index].name != '') {
+        array.push(expectationsForm[index])
+      }
+    }
+    return array
+  }
+
 }
